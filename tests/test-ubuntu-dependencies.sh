25809dc660c57c539e76aea4f741a48240c9c7e4
Zeioth <zeioth@hotmail.com>
2024-02-19 23:21:27 +0100

    ✨feat: Ubuntu GitHub action.

---
+++ tests/test-ubuntu-dependencies.sh
@@ -0,0 +1,260 @@
+#!/usr/bin/env bash
+#
+# NormalNvim test to check if all Ubuntu dependencies still exist.
+
+
+# Imports
+source "$(dirname "$0")/test-utils.sh" # Functions to check packages
+
+
+
+# CHECK DEPENDENCIES
+# -----------------------------------------------------------------------------
+
+
+echo "------------------------------------"
+echo "NormalNvim dependencies - Ubuntu"
+echo "------------------------------------"
+# ubuntu
+echo "- Ubuntu packages -"
+dependencies=(
+    "yarn"
+    "ranger"
+    "rust-fd-find"
+    "python-pynvim"
+    "python-pytest"
+    "delta"
+    "rust-grcov"
+    "rustup"
+)
+check_ubuntu_dependencies "${dependencies[@]}" || exit 1
+
+# npm
+printf '\n%s\n' "- NPM packages -"
+dependencies=(
+    "jest"
+)
+check_npm_dependencies "${dependencies[@]}" || exit 1
+printf '\n\n\n\n'
+
+
+
+
+echo "----------------------------"
+echo "Compiler.nvim dependencies"
+echo "----------------------------"
+# ubuntu
+echo "- Ubuntu packages -"
+dependencies=(
+    "mingw-w64"
+    "dotnet8"
+    "monodevelop"
+    "java-common" # default-jdk
+    "nasm"
+    "r-base"
+    "rustc"
+    "golang"
+    "python"
+    "ruby"
+    "perl"
+    "lua5.3"
+    "kotlin"
+    "elixir"
+    "make"
+    "nodejs"
+    "npm"
+    "node-typescript"
+    "nuitka"
+)
+check_ubuntu_dependencies "${dependencies[@]}" || exit 1
+
+# snap
+printf '\n%s\n' "- Snap packages -"
+dependencies=(
+    "flutter"
+)
+check_snap_dependencies "${dependencies[@]}" || exit 1
+
+# python
+# printf '\n%s\n' "- Python packages -"
+# dependencies=(
+#     "pyinstaller" # Will provoke a warning on ubuntu, since PEP 711
+# )
+# check_python_dependencies "${dependencies[@]}" || exit 1
+
+# You can install dart (but we won't)
+# wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
+# echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
+# sudo apt-get update && sudo apt-get install dart
+
+# You can install flutter (but we won't)
+# sudo snap install dart flutter --classic
+# wget https://swift.org/builds/swift-5.5-release/ubuntu2004/swift-5.5-RELEASE/swift-5.5-RELEASE-ubuntu20.04.tar.gz && tar -xzf swift-5.5-RELEASE-ubuntu20.04.tar.gz && sudo mv swift-5.5-RELEASE-ubuntu20.04 /opt/swift && export PATH=/opt/swift/usr/bin:"${PATH}"
+printf '\n\n\n\n'
+
+echo "------------------------"
+echo "Dooku.nvim dependencies"
+echo "------------------------"
+# pacman
+echo "- Pacman packages -"
+dependencies=(
+    "doxygen"
+)
+check_ubuntu_dependencies "${dependencies[@]}" || exit 1
+
+# npm
+printf '\n%s\n' "- NPM packages -"
+dependencies=(
+    "jsdoc"
+    "typedoc"
+)
+check_npm_dependencies "${dependencies[@]}" || exit 1
+
+# go
+printf '\n%s\n' "- Go packages -"
+dependencies=(
+    "godoc"
+)
+check_go_dependencies "${dependencies[@]}" || exit 1
+printf '\n\n\n\n'
+
+
+
+
+echo "----------------------------------------"
+echo "SUCCESS: All dependencies are available"
+echo "----------------------------------------"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # CHECK DEPENDENCIES
+# # -----------------------------------------------------------------------------
+#
+# echo "------------------------------------"
+# echo "NormalNvim dependencies - Arch Linux"
+# echo "------------------------------------"
+# # pacman
+# echo "- Pacman packages -"
+# dependencies=(
+#     "python"
+#     "python-pynvim"
+#     "fd"
+#     "git-delta"
+#     "grcov"
+#     "rustup"
+#     "yarn"
+#     "python-pytest"
+# )
+# check_arch_dependencies "${dependencies[@]}" || exit 1
+#
+# # npm
+# printf '\n%s\n' "- NPM packages -"
+# dependencies=(
+#     "jest"
+#     "typedoc"
+# )
+# check_npm_dependencies "${dependencies[@]}" || exit 1
+#
+# # cargo
+# printf '\n%s\n' "- Cargo packages -"
+# dependencies=(
+#     "cargo-nextest"
+# )
+# check_cargo_dependencies "${dependencies[@]}" || exit 1
+#
+# printf '\n\n\n\n'
+#
+# echo "----------------------------"
+# echo "Compiler.nvim dependencies"
+# echo "----------------------------"
+# # pacman
+# echo "- Pacman packages -"
+# dependencies=(
+#     "mingw-w64-gcc" # includes mingw-w64-bintools whish has 'ld'
+#     "dotnet-runtime"
+#     "dotnet-sdk"
+#     "aspnet-runtime"
+#     "mono"
+#     "jdk-openjdk"
+#     "dart"
+#     "kotlin"
+#     "elixir"
+#     "npm"
+#     "nodejs"
+#     "typescript"
+#     "make"
+#     "go"
+#     "nasm"
+#     "r"
+#     "nuitka"
+#     "python"
+#     "ruby"
+#     "perl"
+#     "lua"
+# )
+# check_arch_dependencies "${dependencies[@]}" || exit 1
+#
+# # aur
+# printf '\n%s\n' "- AUR packages -"
+# dependencies=(
+#     "pyinstaller"
+#     "swift-bin"
+# )
+# check_aur_dependencies "${dependencies[@]}" || exit 1
+# printf '\n\n\n\n'
+#
+#
+#
+#
+# echo "------------------------"
+# echo "Dooku.nvim dependencies"
+# echo "------------------------"
+# # pacman
+# echo "- Pacman packages -"
+# dependencies=(
+#     "doxygen"
+# )
+# check_arch_dependencies "${dependencies[@]}" || exit 1
+#
+# # npm
+# printf '\n%s\n' "- NPM packages -"
+# dependencies=(
+#     "jsdoc"
+#     "typedoc"
+# )
+# check_npm_dependencies "${dependencies[@]}" || exit 1
+#
+# # go
+# printf '\n%s\n' "- Go packages -"
+# dependencies=(
+#     "godoc"
+# )
+# check_go_dependencies "${dependencies[@]}" || exit 1
+# printf '\n\n\n\n'
+#
+#
+#
+#
+# echo "----------------------------------------"
+# echo "SUCCESS: All dependencies are available"
+# echo "----------------------------------------"
